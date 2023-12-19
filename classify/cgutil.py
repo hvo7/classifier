6cb247f0dddd8baf7adda3ee6bb49ec1a3f497e6
Yuta Kawakubo <ykawakubo@a4-cf-99-4d-06-03.wlan.lsu.edu>
2023-12-19 12:26:39 -0600

    make_spectrum.py was added to the repository.

--- classify/cgutil.py
+++ classify/cgutil.py
@@ -9,6 +9,8 @@ Date: 2023-11-14
 Version: 1.0
 """
 
+
+import numpy as np
 from astropy.time import Time
 from scipy.spatial.transform import Rotation as R
 
@@ -19,6 +21,10 @@ T0 = Time(T0_STR, format="isot", scale="utc")
 # Half of the bin width
 PERIODIC_HBIN = 0.5
 TH_HBIN = 1. / 16.
+PH_HBIN = 2.0
+
+PH_BINNUM_HIGH = 102
+PH_BINNUM_LOW = 410
 
 # Coordinate conversion quaternion
 CGBM2CAL = dict.fromkeys(('HXM1', 'HXM2', 'HXM'),
@@ -29,3 +35,241 @@ CAL2CGBM = dict.fromkeys(('HXM1', 'HXM2', 'HXM'),
 CAL2CGBM['SGM'] = CGBM2CAL['SGM'].inv()
 CAL2ISS = R.from_quat([-0.003635, -0.001678, -0.706655, 0.707194])
 ISS2CAL = CAL2ISS.inv()
+
+# Parameters
+
+# Pedestal
+HXM1H_PED = -2.2
+HXM1H_PED_ERR = 0.1
+HXM2H_PED = -5.25
+HXM2H_PED_ERR = 0.04
+SGMH_PED = 10.41
+SGMH_PED_ERR = 0.03
+HXM1L_PED = -1.24
+HXM1L_PED_ERR = 0.04
+HXM2L_PED = -0.475
+HXM2L_PED_ERR = 0.002
+SGML_PED = -0.171
+SGML_PED_ERR = 0.003
+
+HXM1_AMP_GAIN=30.458575
+HXM2_AMP_GAIN=29.935845
+SGM_AMP_GAIN=30.381555
+
+
+LINR_HXML = 0.75
+LINR_SGML = 7.0
+LINR_HXMH = 0.024
+LINR_SGMH = 0.23
+
+GC_LINE = 511.0
+GC_LINE_HXM = 1470.0
+GC_LINE_SGM = 2200.0
+
+
+PH_LLIM_HXMH = 57 #>30.2keV
+PH_ULIM_HXMH = 89 #<80.9keV
+PH_LLIM_HXML = 51 #>81.0keV
+PH_ULIM_HXML = 320 #<2004.0keV
+
+PH_LLIM_SGMH = 33 #>101.9keV
+PH_ULIM_SGMH = 81 #<657.6keV
+PH_LLIM_SGML = 47 #>658.0keV
+PH_ULIM_SGML = 332 #<20048.0keV
+
+def high_th_def():
+    """
+    This function makes ADC edge of TH High gain.
+    """
+    th_def = np.array(
+        [[155, 410],
+         [411, 1002],
+         [1003, 2026],
+         [2027, 4031]])
+    return th_def
+
+def low_th_def():
+    """
+    This function makes ADC edge of TH Low gain.
+    """
+    th_def = np.array(
+        [[80, 119],
+          [120, 219],
+          [220, 383],
+         [384, 4079]])
+    return th_def
+
+def high_th_edge():
+    """
+    This function makes ADC edge of TH High gain.
+    """
+    th_def = np.array(
+        [[155, 411],
+         [411, 1003],
+         [1003, 2027],
+         [2027, 4032]])
+    return th_def
+
+def low_th_edge():
+    """
+    This function makes ADC edge of TH Low gain.
+    """
+    th_def = np.array(
+        [[80, 120],
+          [120, 220],
+          [220, 384],
+         [384, 4080]])
+    return th_def
+
+
+
+def high_ph():
+    """
+    This function makes ADC edge of PH High gain.
+    """
+
+    bin_1 = np.array([0])
+    bin_2 = np.arange(1, 2, 2)
+    bin_4 = np.arange(3, 10, 4)
+    bin_8 = np.arange(11, 42, 8)
+    bin_16 = np.arange(43, 682, 16)
+    bin_64 = np.arange(683, 4075, 64)
+    bin_21 = np.array([4075, 4096])
+
+    bin_all = np.concatenate(
+            [bin_1, bin_2, bin_4, bin_8, bin_16, bin_64, bin_21],
+            axis=0)
+    return bin_all
+
+
+def low_ph():
+    """
+    This function makes ADC edge of PH High gain.
+    """
+
+    bin_2 = np.arange(0, 95, 2)
+    bin_4 = np.arange(96, 351, 4)
+    bin_8 = np.arange(352, 1375, 8)
+    bin_16 = np.arange(1376, 4095, 16)
+    last = np.array([4096])
+
+    bin_all = np.concatenate([bin_2, bin_4, bin_8, bin_16, last], axis=0)
+    return bin_all
+
+
+def high_ph_width():
+    ph_edge = high_ph()
+    width = ph_edge[1:] - ph_edge[:-1]
+    return width
+
+
+def low_ph_width():
+    ph_edge = low_ph()
+    width = ph_edge[1:] - ph_edge[:-1]
+    return width
+
+
+def high_ph_center():
+    ph_edge = high_ph()
+    width = high_ph_width()
+    center = ph_edge[:-1] + width/2.
+    return center
+
+
+def low_ph_center():
+    ph_edge = low_ph()
+    width = low_ph_width()
+    center = ph_edge[:-1] + width/2.
+    return center
+
+
+def func0(x, a):
+    """
+    func0 is definition 0-order function of x.
+    """
+    return a + x*0.
+
+
+def func1(x, a, b):
+    """
+    func1 is definition 1-order function of x.
+    """
+    return b*x + a
+
+
+def func2(x, a, b, c):
+    """
+    func2 is definition 2-order function of x.
+    """
+    return c*(x**2.0) + b*x + a
+
+
+def func3(x, a, b, c, d):
+    """
+    func3 is definition 3-order function of x.
+    """
+    return d*(x**3.0) + c*(x**2.0) + b*x + a
+
+def gauss(x,p0, p1, p2, p3, p4):
+    y = p0*(np.exp(-((x-p1)**2)/(2*p2))) + p3*x + p4
+    return y
+
+
+def gauss_alt(x, p0, p1, p2, p3, p4):
+    y = (
+        (2.355*p0)
+        / (p1*p2 * np.sqrt(2.0 * np.pi))
+        * np.exp((-1.0/2.0) * ((2.355*(x-p1))/(p1 * p2)) ** 2.0)
+        + p3*x
+        + p4
+        )
+    return y
+
+
+def adc2ene_ngc(det, adc_chan):
+    if det == 'HXM1H':
+        ene = (adc_chan-HXM1H_PED) * LINR_HXMH
+
+    if det == 'HXM2H':
+        ene = (adc_chan-HXM2H_PED) * LINR_HXMH
+
+    elif det == 'SGMH':
+        ene = (adc_chan-SGMH_PED) * LINR_SGMH
+
+    elif det == 'HXM1L':
+        ene = (adc_chan-HXM1L_PED) * LINR_HXML
+
+    elif det == 'HXM2L':
+        ene = (adc_chan-HXM2L_PED) * LINR_HXML
+
+    elif det == 'SGML':
+        ene = (adc_chan-SGML_PED) * LINR_SGML
+
+    else:
+        raise ValueError
+        sys.exit()
+    return ene
+
+def adc2ene_gc(det, adc_chan):
+    if det == 'HXM1H':
+        ene = adc_chan * LINR_HXMH
+
+    elif det == 'HXM2H':
+        ene = adc_chan * LINR_HXMH
+
+    elif det == 'SGMH':
+        ene = adc_chan * LINR_SGMH
+
+    elif det == 'HXM1L':
+        ene = adc_chan * LINR_HXML
+
+    elif det == 'HXM2L':
+        ene = adc_chan * LINR_HXML
+
+    elif det == 'SGML':
+        ene = adc_chan * LINR_SGML
+
+    else:
+        raise ValueError
+        sys.exit()
+    return ene
---
+++ classify/make_spectrum.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+Script Name: make_spectrum.py
+Description: This script makes spectra of each detector from PH data and save images of spectra.
+
+Author: Yuta Kawakubo
+Date: 2023-12-19
+Version: 1.0
+"""
+
+from astropy.table import Table, vstack
+import numpy as np
+import cgutil as cg
+import matplotlib.pyplot as plt
+from dotenv import load_dotenv
+import os
+import sys
+
+#Input line
+args = sys.argv
+
+if len(args) != 4:
+    print("Usage: python ./make_spectrum.py fileid(yyyymmdd) chan_type(PHA/PI) ref_met(offset MET)")
+    sys.exit(1)
+
+else:
+    fileid = str(args[1])
+    chan_type = str(args[2])
+    ref_met = float(args[3])
+
+
+def get_phdata(fileid, data_dir, chan_type):
+    if not (chan_type == 'PHA' or chan_type == 'PI'):
+        print('chan_type is not expected.')
+        return None
+
+    hxm1h_ph_file = '%s/cgbm_%s_hx1_hph.fits' % (data_dir, fileid)
+    hxm1h_ph_table = Table.read(hxm1h_ph_file, hdu=1)
+    hxm1l_ph_file = '%s/cgbm_%s_hx1_lph.fits' % (data_dir, fileid)
+    hxm1l_ph_table = Table.read(hxm1l_ph_file, hdu=1)
+
+    hxm2h_ph_file = '%s/cgbm_%s_hx2_hph.fits' % (data_dir, fileid)
+    hxm2h_ph_table = Table.read(hxm2h_ph_file, hdu=1)
+    hxm2l_ph_file = '%s/cgbm_%s_hx2_lph.fits' % (data_dir, fileid)
+    hxm2l_ph_table = Table.read(hxm2l_ph_file, hdu=1)
+
+    sgmh_ph_file = '%s/cgbm_%s_sgm_hph.fits' % (data_dir, fileid)
+    sgmh_ph_table = Table.read(sgmh_ph_file, hdu=1)
+    sgml_ph_file = '%s/cgbm_%s_sgm_lph.fits' % (data_dir, fileid)
+    sgml_ph_table = Table.read(sgml_ph_file, hdu=1)
+
+    # 'TIME' is Misson Elapsed Time (MET) same as Suzaku/MAXI
+    # MET = 0 is 2000-01-01T00:00:00 UTC
+    # 'MDCTIME' is time in MDC time
+    merged = hxm1h_ph_table[['TIME', 'MDCTIME']]
+
+    # '*_EXPOSURE's are exposure for each detector.
+    merged['HXM1_EXPOSURE'] = hxm1h_ph_table['EXPOSURE']
+    merged['HXM2_EXPOSURE'] = hxm2h_ph_table['EXPOSURE']
+    merged['SGM_EXPOSURE'] = sgmh_ph_table['EXPOSURE']
+
+    # '*_COUNTS's are counts per PH time binning (4s)
+    merged['HXM1H_COUNTS'] = hxm1h_ph_table[chan_type]
+    merged['HXM1L_COUNTS'] = hxm1l_ph_table[chan_type]
+    merged['HXM2H_COUNTS'] = hxm2h_ph_table[chan_type]
+    merged['HXM2L_COUNTS'] = hxm2l_ph_table[chan_type]
+    merged['SGMH_COUNTS'] = sgmh_ph_table[chan_type]
+    merged['SGML_COUNTS'] = sgml_ph_table[chan_type]
+
+    merged['HXM1H_RATE'] = merged['HXM1H_COUNTS'] / np.tile(merged['HXM1_EXPOSURE'], (cg.PH_BINNUM_HIGH, 1)).T
+    merged['HXM2H_RATE'] = merged['HXM2H_COUNTS'] / np.tile(merged['HXM2_EXPOSURE'], (cg.PH_BINNUM_HIGH, 1)).T
+    merged['SGMH_RATE'] = merged['SGMH_COUNTS'] / np.tile(merged['SGM_EXPOSURE'], (cg.PH_BINNUM_HIGH, 1)).T
+    merged['HXM1L_RATE'] = merged['HXM1L_COUNTS'] / np.tile(merged['HXM1_EXPOSURE'], (cg.PH_BINNUM_LOW, 1)).T
+    merged['HXM2L_RATE'] = merged['HXM2L_COUNTS'] / np.tile(merged['HXM2_EXPOSURE'], (cg.PH_BINNUM_LOW, 1)).T
+    merged['SGML_RATE'] = merged['SGML_COUNTS'] / np.tile(merged['SGM_EXPOSURE'], (cg.PH_BINNUM_LOW, 1)).T
+
+    return merged
+
+
+def main(fileid, chan_type, ref_met):
+    # Input example
+    #fileid = '20231110'
+    #chan_type = 'PHA'
+    #ref_met = 752966385.210472
+
+    load_dotenv()
+    data_dir = os.getenv("LOCAL_DATA_DIR")
+    ph_table = get_phdata(fileid, data_dir, chan_type)
+    ph_table['ZTIME'] = ph_table['TIME'] - cg.PH_HBIN - ref_met
+
+
+    fig, cv = plt.subplots(3, 1, sharex=True, figsize=(8, 6))
+    cv[0].set_xlim(-1800, 1800)
+    cv[0].plot(ph_table['ZTIME'], np.sum(ph_table['HXM1L_COUNTS'], axis=1), drawstyle='steps-mid', label='HXM1')
+    cv[1].plot(ph_table['ZTIME'], np.sum(ph_table['HXM2L_COUNTS'], axis=1), drawstyle='steps-mid', label='HXM2')
+    cv[2].plot(ph_table['ZTIME'], np.sum(ph_table['SGML_COUNTS'], axis=1), drawstyle='steps-mid', label='SGM')
+
+    cv[2].set_xlabel('Time [s] since %.6f' % (ref_met), fontsize=14)
+    plt.subplots_adjust(hspace=0, top=0.95, bottom=0.1, left=0.15, right=0.95)
+
+    for ax in cv:
+        ax.axvline(x=0, color='red', linestyle='--')
+        ax.tick_params(labelsize=14)
+        ax.set_ylabel('Counts/s', fontsize=14)
+
+    plt.legend(loc='best')
+    plt.show()
+
+    tmin = float(input("Please input the start time:"))
+    tmax = float(input("Please input the end time:"))
+
+    tmask = (tmin <= ph_table['ZTIME']) & (ph_table['ZTIME'] <= tmax)
+    roi = ph_table[tmask]
+
+    # HXM1H
+    fig = plt.figure(figsize=(8, 6))
+    plt.plot(
+        np.arange(cg.PH_BINNUM_HIGH),
+        np.sum(roi['HXM1H_RATE'], axis=0) / cg.high_ph_width(),
+        drawstyle='steps-mid')
+    plt.yscale('log')
+    plt.xlabel('PH channel')
+    plt.ylabel('Counts / ADC channel')
+    plt.title('HXM1H spectrum')
+    fig.savefig('%s_hxm1h_spec.png' % (fileid))
+    plt.show()
+
+    # HXM2H
+    fig = plt.figure(figsize=(8, 6))
+    plt.plot(
+        np.arange(cg.PH_BINNUM_HIGH),
+        np.sum(roi['HXM2H_RATE'], axis=0) / cg.high_ph_width(),
+        drawstyle='steps-mid')
+    plt.yscale('log')
+    plt.xlabel('PH channel')
+    plt.ylabel('Counts / ADC channel')
+    plt.title('HXM2H spectrum')
+    fig.savefig('%s_hxm2h_spec.png' % (fileid))
+    plt.show()
+
+    # SGMH
+    fig = plt.figure(figsize=(8, 6))
+    plt.plot(
+        np.arange(cg.PH_BINNUM_HIGH),
+        np.sum(roi['SGMH_RATE'], axis=0) / cg.high_ph_width(),
+        drawstyle='steps-mid')
+    plt.yscale('log')
+    plt.xlabel('PH channel')
+    plt.ylabel('Counts / ADC channel')
+    plt.title('SGMH spectrum')
+    fig.savefig('%s_sgmh_spec.png' % (fileid))
+    plt.show()
+
+    # HXM1L
+    fig = plt.figure(figsize=(8, 6))
+    plt.plot(
+        np.arange(cg.PH_BINNUM_LOW),
+        np.sum(roi['HXM1L_RATE'], axis=0) / cg.low_ph_width(),
+        drawstyle='steps-mid')
+    plt.yscale('log')
+    plt.xlabel('PH channel')
+    plt.ylabel('Counts / ADC channel')
+    plt.title('HXM1L spectrum')
+    fig.savefig('%s_hxm1l_spec.png' % (fileid))
+    plt.show()
+
+    # HXM2H
+    fig = plt.figure(figsize=(8, 6))
+    plt.plot(
+        np.arange(cg.PH_BINNUM_LOW),
+        np.sum(roi['HXM2L_RATE'], axis=0) / cg.low_ph_width(),
+        drawstyle='steps-mid')
+    plt.yscale('log')
+    plt.xlabel('PH channel')
+    plt.ylabel('Counts / ADC channel')
+    plt.title('HXM2L spectrum')
+    fig.savefig('%s_hxm2l_spec.png' % (fileid))
+    plt.show()
+
+    # SGMH
+    fig = plt.figure(figsize=(8, 6))
+    plt.plot(
+        np.arange(cg.PH_BINNUM_LOW),
+        np.sum(roi['SGML_RATE'], axis=0) / cg.low_ph_width(),
+        drawstyle='steps-mid')
+    plt.yscale('log')
+    plt.xlabel('PH channel')
+    plt.ylabel('Counts / ADC channel')
+    plt.title('SGML spectrum')
+    fig.savefig('%s_sgml_spec.png' % (fileid))
+    plt.show()
+
+if __name__ == "__main__":
+    main(fileid, chan_type, ref_met)
